a4496eab1ddf2c9713ac64ee51caf8713c6c230b
lightsong <qsfan@qq.com>
2021-03-02 15:39:59 +0800

    add hub code

--- machine_learning/hub/arrary.py
+++ machine_learning/hub/arrary.py
@@ -5,7 +5,7 @@ mnist = Dataset("activeloop/mnist")  # loading the MNIST data lazily
 # saving time with *compute* to retrieve just the necessary data
 
 print(type(mnist))
-print(dir(mnist))
+#print(dir(mnist))
 #pprint(mnist.__dict__)
 #
 print("-----------------------------------")
@@ -13,20 +13,20 @@ print("-----------------------------------")
 
 data = mnist["image"][0:1000].compute()
 
-print(dir(data))
+#print(dir(data))
 
 print(type(data))
 print(data.shape)
-print(data[0])
+#print(data[0])
 
 
 data = mnist["label"][0:1000].compute()
 
-print(dir(data))
+#print(dir(data))
 
 print(type(data))
 print(data.shape)
-print(data[0])
+#print(data[0])
 
 
 
