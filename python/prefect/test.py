fe7e79a2d30a21db33e9e19e386387e4ef09255f
qsfan <qsfan@qq.com>
2021-07-20 16:42:35 +0800

    add prefect code

---
+++ python/prefect/test.py
@@ -0,0 +1,16 @@
+from prefect import task, Flow, Parameter
+
+
+@task(log_stdout=True)
+def say_hello(name):
+    print("Hello, {}!".format(name))
+
+
+with Flow("My First Flow") as flow:
+    name = Parameter('name')
+    say_hello(name)
+
+
+flow.run(name='world') # "Hello, world!"
+flow.run(name='Marvin') # "Hello, Marvin!"
+
