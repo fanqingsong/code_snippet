e55523a27ae348c19bc53d08cec6af96416db554
qsfan <qsfan@qq.com>
2021-11-30 09:26:31 +0800

    alchemy code

---
+++ python/sqlalchemy/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column,Integer,String,create_engine,Text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ ='user'
+
+    id = Column(Integer,primary_key=True)   
+
+    username = Column(String(20),nullable=False)   
+
+    password = Column(String(100),nullable=False)
+
+
+class Article(Base):
+    __tablename__ ='article'
+
+    id = Column(Integer,primary_key=True)   
+
+    title = Column(String(100),nullable=False)   
+
+    content = Column(Text, nullable=False)
