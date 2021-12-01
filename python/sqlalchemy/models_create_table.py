350d45ed058cafb155ee8f9bcf0cda634f1fe873
qsfan <qsfan@qq.com>
2021-12-01 15:55:27 +0800

    alembic integration with mysql

--- python/sqlalchemy/models_create_table.py
+++ python/sqlalchemy/models_create_table.py
@@ -3,7 +3,8 @@ from models_definition import Base
 
 
 # 初始化数据库连接:
-engine = create_engine('sqlite:///test.db')
+# engine = create_engine('sqlite:///test.db')
+engine = create_engine('mysql+mysqldb://root:root@localhost/playground')
 
 Base.metadata.create_all(engine)
 
