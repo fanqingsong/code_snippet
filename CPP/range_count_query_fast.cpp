23d3032069887de2ad3683956da74c689b9ca2d8
qsfan <qsfan@qq.com>
2022-04-21 10:31:09 +0800

    add range count query code

--- CPP/range_count_query_fast.cpp
+++ CPP/range_count_query_fast.cpp
@@ -14,7 +14,7 @@ https://atcoder.jp/contests/abc248/tasks/abc248_d
 
 int main() {
 	int N;
-	map<int, set<int>> gp;
+	map<int, vector<int>> gp;
 
 	int Q;
 
@@ -22,7 +22,7 @@ int main() {
 	for (int i = 1; i <= N; i++) {
 		int temp;
 		cin >> temp;
-		gp[temp].insert(i);
+		gp[temp].push_back(i);
 	}
 
 	cin >> Q;
@@ -31,16 +31,59 @@ int main() {
 		int L, R, X;
 		cin >> L >> R >> X;
 
-		set<int> &index_list = gp[X];
+		vector<int> &index_list = gp[X];
+
+		int low_index = -1;
+		int up_index = -1;
+		int size = index_list.size();
+
+		int low = 0;
+		int up = size - 1;
+		int middle = 0;
+		int mid_value = 0;
+		while (low <= up) {
+			middle = (low + up) / 2;
+			mid_value = index_list[middle];
+			if (mid_value == L) {
+				low_index = middle;
+				break;
+			} else if (mid_value < L) {
+				low = middle + 1;
+			} else if (mid_value > L) {
+				up = middle - 1;
+			}
+		}
+
+		if (low_index == -1) {
+			// up = low - 1
+			// means: index_list[up] < L < index_list[low]
+			low_index = low;
+		}
+
+		low = 0;
+		up = size - 1;
+		middle = 0;
+		while (low <= up) {
+			middle = (low + up) / 2;
+			mid_value = index_list[middle];
+			if (mid_value == R) {
+				up_index = middle;
+				break;
+			} else if (mid_value < R) {
+				low = middle + 1;
+			} else if (mid_value > R) {
+				up = middle - 1;
+			}
+		}
+
+		if (up_index == -1) {
+			// up = low - 1
+			// means: index_list[up] < R < index_list[low]
+			up_index = up;
+		}
 
 		int count = 0;
-		set<int>::iterator itlow, itup;
-		itlow = index_list.lower_bound (L);              //       ^
-		itup = index_list.upper_bound (R);               //                   ^
-
-		for (set<int>::iterator it = itlow; it != itup; ++it) {
-			count ++;
-		}
+		count = up_index - (low_index - 1);
 
 		cout << count << endl;
 	}
