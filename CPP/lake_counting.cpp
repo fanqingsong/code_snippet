7bb1ec04e9b6ccec8c4d057afe06ba87f7c26736
qsfan <qsfan@qq.com>
2022-06-05 14:31:53 +0800

    lake counting

--- CPP/lake_counting.cpp
+++ CPP/lake_counting.cpp
@@ -8,13 +8,73 @@
 using namespace std;
 
 /*
-http://xinao.ainoob.cn/article/529
+http://poj.org/problem?id=2386
 */
 
+bool wfields[102][102] = {{false}};
+
+bool visited[102][102] = {{false}};
+int n, m;
+
+
+void dfs(int i, int j) {
+	if (i < 1 || i > n ) {
+//		cout << "i is out of bound. i=" << i << endl;
+		return;
+	}
+
+	if (j < 1 || j > m) {
+//		cout << "j is out of bound. j=" << j << endl;
+		return;
+	}
+
+	if (visited[i][j] == true) {
+		return ;
+	} else {
+		visited[i][j] = true;
+	}
+
+	if (wfields[i][j] == false) {
+//		cout << "water fields is false." << endl;
+		return;
+	}
+
+	for (int diff_i = -1; diff_i <= 1; diff_i++) {
+		for (int diff_j = -1; diff_j <= 1; diff_j++) {
+			dfs(i + diff_i, j + diff_j);
+		}
+	}
+}
+
+
 int main() {
-	int n;
+	cin >> n >> m;
 
-	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		string temp;
+		cin >> temp;
 
-	cout << char(n) << endl;
+		for (int j = 1; j <= m; j++) {
+			if (temp[j - 1] == 'W') {
+				wfields[i][j] = true;
+			} else {
+				wfields[i][j] = false;
+			}
+
+			visited[i][j] = false;
+		}
+	}
+
+	int count = 0;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (wfields[i][j] == true && visited[i][j] == false) {
+				count ++;
+
+				dfs(i, j);
+			}
+		}
+	}
+
+	cout << count << endl;
 }
