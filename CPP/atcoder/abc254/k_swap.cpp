be2f1213471b3882f64130127276d43827ab41e0
qsfan <qsfan@qq.com>
2022-06-06 22:32:31 +0800

    k swap

--- CPP/atcoder/abc254/k_swap.cpp
+++ CPP/atcoder/abc254/k_swap.cpp
@@ -14,6 +14,9 @@ https://atcoder.jp/contests/abc254/tasks/abc254_c
 
 vector<int> a(200004, 0);
 
+map<int, vector<int>> groups;
+
+
 int main() {
 	int n, k;
 
@@ -21,7 +24,49 @@ int main() {
 
 	for (int i = 1; i <= n; i++) {
 		cin >> a[i];
+
+		/*
+		group indexed from 0 to k-1
+		1  --> group 0
+		2 --> group 1
+		...
+		k --> group k-1
+		k+1 --> group 0
+		k+2 --> group 1
+		...
+		*/
+		int group_index = (i - 1) % k;
+		groups[group_index].push_back(a[i]);
 	}
 
+	for (int i = 0; i < k; i++) {
+		vector<int> &one_group = groups[i];
+		sort(one_group.begin(), one_group.end());
+//
+//		for (int j = 0; j < one_group.size(); j++) {
+//			cout << one_group[j] << " " ;
+//		}
+//
+//		cout << endl;
+	}
 
+	int prev = groups[0][0];
+	for (int i = 2; i <= n; i++) {
+		int group_num = (i - 1) % k;
+		int column = (i - 1) / k;
+
+		int now = groups[group_num][column];
+		if (prev <= now) {
+			prev = now;
+			continue;
+		} else {
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+
+	cout << "Yes" << endl;
+	return 0;
 }
+
+
